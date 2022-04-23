7482c541f85b48de22a48d6105f3c3b53210cf5a
Joshua Chen <sidachen2003@gmail.com>
2022-04-23 20:21:29 +0800

    fix(create-jc-project): better error handling

--- packages/create-jc-project/src/index.ts
+++ packages/create-jc-project/src/index.ts
@@ -6,16 +6,34 @@ import { fileURLToPath } from "node:url";
 import path from "node:path";
 import cp, { type SpawnSyncReturns } from "node:child_process";
 
-function exec(command: string) {
+function exec(
+  command: string,
+  options: { times?: number; exitEarly?: boolean } = {},
+) {
+  const { times = 1, exitEarly = false } = options;
   console.log(pico.gray(`$ ${command}`));
-  try {
-    cp.execSync(command, { encoding: "utf-8", stdio: "inherit" });
-  } catch (err) {
-    if (((e): e is SpawnSyncReturns<string> => true)(err)) {
-      if (err.stdout) console.log(err.stdout);
-      if (err.stderr) console.log(err.stderr);
-      console.log(`Command exited with code ${err.status}`);
+  function tryOnce(v: boolean, index: number) {
+    try {
+      cp.execSync(command, { encoding: "utf-8", stdio: "inherit" });
+      return true;
+    } catch (err) {
+      if (((e): e is SpawnSyncReturns<string> => true)(err)) {
+        if (err.stdout) console.log(err.stdout);
+        if (err.stderr) console.log(err.stderr);
+        console.log(pico.red(`Command exited with code ${err.status}.`));
+        if (index < times - 1) console.log(pico.cyan("Retrying..."));
+      }
+      return false;
     }
+  }
+  const success = Array.from({ length: times }, () => true).some(tryOnce);
+  if (!success) {
+    if (exitEarly) process.exit(1);
+    console.log(
+      pico.cyan(
+        `Continuing to the next step. You can re-run "${command}" later on yourself.`,
+      ),
+    );
   }
 }
 
@@ -184,6 +202,7 @@ if (usingReact) {
 await fs.writeFile(".eslintrc", JSON.stringify(eslintRC, null, 2));
 await fs.writeFile("tsconfig.json", JSON.stringify(tsconfig, null, 2));
 
-exec("yarn set version stable");
+// This often fails. Let's try multiple times.
+exec("yarn set version stable", { times: 3 });
 exec(`yarn add -D ${dependencies.sort().join(" ")}`);
 exec("yarn prepare");
